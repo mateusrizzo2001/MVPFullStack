f5772552dbd20c5d52c9d88dca6206fe23c4909a
Mateus Rizzo <mateusrebellorizzo@gmail.com>
2023-05-03 20:19:05 -0300

    final-commit

--- API/app.py
+++ API/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, render_template, make_response
 from flask_swagger import swagger 
 import sqlite3
 
@@ -6,17 +6,38 @@ app = Flask(__name__)
 
 banco = sqlite3.connect('mvp_livros.db')
 cursor = banco.cursor()
-cursor.execute('CREATE TABLE IF NOT EXISTS livros_registrados (titulo TEXT, autor TEXT, ano INTEGER)')
+cursor.execute('CREATE TABLE IF NOT EXISTS livros_registrados (titulo TEXT, autor TEXT, ano INTEGER, id INTEGER PRIMARY KEY AUTOINCREMENT)')
 
 @app.route("/swagger")
 def get_swagger():
-    swag = swagger(app)
+    swag = swagger(app, prefix='/v1')
     swag['info']['title'] = "API de livros"
     swag['info']['version'] = "1.0"
     return swag
 
-@app.route('/cadastrar_livro', methods=['POST'])
+@app.route('/v1/cadastrar_livro', methods=['POST'])
 def cadastrar_livro():
+    """
+    Cadastra um novo livro no banco de dados.
+
+    ---
+    parameters:
+      - name: titulo
+        in: formData
+        type: string
+        required: true
+      - name: autor
+        in: formData
+        type: string
+        required: true
+      - name: ano
+        in: formData
+        type: integer
+        required: true
+    responses:
+      200:
+        description: Livro cadastrado com sucesso.
+    """
     titulo = request.form['titulo']
     autor = request.form['autor']
     ano = request.form['ano']
@@ -24,22 +45,65 @@ def cadastrar_livro():
     banco.commit()
     return 'Livro cadastrado com sucesso!'
 
-@app.route('/buscar_livro', methods=['GET'])
+@app.route('/v1/buscar_livro', methods=['GET'])
 def buscar_livro():
+    """
+    Busca um livro pelo ID.
+
+    ---
+    parameters:
+      - name: id
+        in: query
+        type: integer
+        required: true
+    responses:
+      200:
+        description: Livro encontrado.
+      404:
+        description: Livro não encontrado.
+    """
     id = request.args.get('id')
-    cursor.execute('SELECT * FROM livros_registrados WHERE rowid = ?', (id,))
+    cursor.execute('SELECT * FROM livros_registrados WHERE id = ?', (id,))
     livro = cursor.fetchone()
     if livro:
         return f'Livro encontrado: {livro}'
     else:
-        return 'Livro não encontrado'
+        return 'Livro não encontrado', 404
 
-@app.route('/deletar_livro', methods=['POST'])
+@app.route('/v1/deletar_livro', methods=['POST'])
 def deletar_livro():
+    """
+    Deleta um livro pelo ID.
+
+    ---
+    parameters:
+      - name: id
+        in: formData
+        type: integer
+        required: true
+    responses:
+      200:
+        description: Livro deletado com sucesso.
+      404:
+        description: Livro não encontrado.
+    """
     id = request.form['id']
-    cursor.execute('DELETE FROM livros_registrados WHERE rowid = ?', (id,))
+    cursor.execute('DELETE FROM livros_registrados WHERE id = ?', (id,))
     banco.commit()
-    return 'Livro deletado com sucesso!'
+    if cursor.rowcount > 0:
+        return 'Livro deletado com sucesso!'
+    else:
+        return 'Livro não encontrado', 404
+
+@app.route('/')
+def home():
+    cursor.execute('SELECT * FROM livros_registrados')
+    livros = cursor.fetchall()
+    response = make_response(render_template('index.html', livros=livros))
+    response.headers['Cache-Control'] = 'no-cache'
+    return response
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
+
